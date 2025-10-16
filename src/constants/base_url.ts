0b000a9159b976bbc7697407cb2b0322f9561c95
ibros1 <maxamedsuhayb41@gmail.com>
2025-10-16 18:04:52 +0300

    base url changed

--- src/constants/base_url.ts
+++ src/constants/base_url.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "https://booking-app-api-umber.vercel.app/api";
+export const BASE_API_URL = "https://booking-app-a-pi.vercel.app/api";
 // export const BASE_API_URL = "http://localhost:7000/api";
